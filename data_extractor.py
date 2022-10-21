7cc85eb9966aa38da2d6cf8a4052cb897ff15915
yololars <larsengoemonsen@gmail.com>
2022-10-21 13:08:42 +0200

    added kinetic energy computation to the data_extractor.py file

--- data_extractor.py
+++ data_extractor.py
@@ -2,7 +2,11 @@ import matplotlib.pyplot as plt
 file = 'lab_data.txt'
 f = open(file, 'r')
 data_list = f.readlines()
-time_data, x_data, y_data, v_data = [],[],[],[]
+time_data, x_data, y_data, v_data, ek_data = [],[],[],[],[]
+m = 0.031
+r = 0.011
+I = (2/5)*m*(r**2)
+
 for i in range(2,len(data_list)):
     data_list[i] = data_list[i].replace(',','.')
     data = data_list[i].strip('\n').split('\t')
@@ -10,6 +14,11 @@ for i in range(2,len(data_list)):
     x_data.append(float(data[1]))
     y_data.append(float(data[2]))
     v_data.append(float(data[3]))
+    omega = float(data[3])/r
+    e_kinetic = (1/2)*m*(float(data[3])**2)
+    e_rotation = (1/2)*I*(omega**2)
+    ek_data.append(e_kinetic+e_rotation)
+
 
 #banens form
 posisjon = plt.figure('y(x)', figsize=(12, 6))
@@ -20,24 +29,36 @@ plt.ylabel('$y$ (m)', fontsize=20)
 plt.grid()
 plt.show()
 
-#farten
-fart = plt.figure('y(x)', figsize=(12, 6))
-plt.plot(x_data, v_data)
-plt.title('Farten')
-plt.xlabel('$x$ (m)', fontsize=20)
-plt.ylabel('$v()$ (m/s)', fontsize=20)
-plt.grid()
-plt.show()
-
-
 #farten som funskjon av tid
 farttid = plt.figure('y(x)', figsize=(12, 6))
 plt.plot(time_data, v_data)
 plt.title('Farten')
 plt.xlabel('$x$ (m)', fontsize=20)
-plt.ylabel('$v()$ (m/s)', fontsize=20)
+plt.ylabel('$v$ (m/s)', fontsize=20)
 plt.grid()
 plt.show()
+
+
+#farten
+fart = plt.figure('y(x)', figsize=(12, 6))
+plt.plot(x_data, v_data)
+plt.title('Farten')
+plt.xlabel('$x$ (m)', fontsize=20)
+plt.ylabel('$v$ (m/s)', fontsize=20)
+plt.grid()
+plt.show()
+
+#kinetisk energi
+kinetiskenergi = plt.figure('y(x)', figsize=(12, 6))
+plt.plot(x_data, ek_data)
+plt.title('Kinetisk energi')
+plt.xlabel('$x$ (m)', fontsize=20)
+plt.ylabel('$EK$ (J)', fontsize=20)
+plt.grid()
+plt.show()
+
+
+
 
 
 
